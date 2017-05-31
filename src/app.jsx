60970c6f54f1f3049f40d165df65d4290b0b514e
skiboy209 <dallanrocks@gmail.com>
2017-05-31 03:20:15 -0600

    Web Baseplate Added

---
+++ src/app.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/index.scss';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>It Works!</h1>
+        <p>Exemplar story in which twleve Vastans materialized a great empire of crystallized sentient beings <span className="redBg">module</span> local styles.</p>
+        <p>Enjoy!</p>
+      </div>
+    )
+  }
+}
